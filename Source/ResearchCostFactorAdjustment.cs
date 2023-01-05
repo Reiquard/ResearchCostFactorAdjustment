263e0ac67b3a13e86372c93cc033e0a6539b7707
Reiquard <62316596+Reiquard@users.noreply.github.com>
2023-01-05 05:11:15 +0300

    Add files via upload

--- Source/ResearchCostFactorAdjustment.cs
+++ Source/ResearchCostFactorAdjustment.cs
@@ -10,11 +10,16 @@ using Verse;
 namespace ResearchCostFactorAdjustment
 {
     [StaticConstructorOnStartup]
-    internal static class HarmonyInit
+    internal static class ResearchCostFactorAdjustment
     {
-        static HarmonyInit()
+        public static bool TechAdvancingIsActive { get; private set; }
+        static ResearchCostFactorAdjustment()
         {
             new Harmony("reiquard.researchcostfactoradjustment").PatchAll();
+            if (ModLister.GetActiveModWithIdentifier("GHXX.TechAdvancing") != null)
+            {
+                TechAdvancingIsActive = true;
+            }
         }
     }
 
@@ -22,18 +27,35 @@ namespace ResearchCostFactorAdjustment
     [HarmonyPatch("CostFactor")]
     static class CostFactorRedefining
     {
-        [HarmonyPrefix]
-        static bool CostFactor_Prefix(TechLevel researcherTechLevel, ResearchProjectDef __instance, ref float __result)
+        private static SimpleCurve _curvePoints = new SimpleCurve
         {
-            TechLevel techLevel = (TechLevel)Mathf.Min((int)__instance.techLevel, (int)RCFA_Mod.settings.threshold);
-            if ((int)researcherTechLevel >= (int)techLevel)
+            new CurvePoint(-20f, 0.4f),
+            new CurvePoint(-4f, 0.6f),
+            new CurvePoint(0f, 1f)
+        };
+        [HarmonyAfter(new string[] { "com.ghxx.rimworld.techadvancing" })]
+        static void Postfix(TechLevel researcherTechLevel, ResearchProjectDef __instance, ref float __result)
+        {
+            float techLevel = (float)Mathf.Min((int)__instance.techLevel, (int)RCFA_Mod.settings.threshold);
+            float dif = techLevel - (float)researcherTechLevel;
+            float difInv = (float)__instance.techLevel - (float)researcherTechLevel;
+            float result = 1f;
+            if (dif > 0)
             {
-                __result = 1f;
-                return false;
+                if (RCFA_Mod.settings.exponential)
+                {
+                    result = (float)Math.Round((1f + RCFA_Mod.settings.factor) * (Math.Pow(dif * 1.442f, dif * 0.5f)), 0);
+                }
+                else
+                {
+                    result = (float)Math.Round(1f + dif * RCFA_Mod.settings.factor, 1);
+                }
             }
-            int num = techLevel - researcherTechLevel;
-            __result = (float)Math.Round(1f + (float)num * (RCFA_Mod.settings.exponential ? (float)num : 1f) * RCFA_Mod.settings.factor, 1);
-            return false;
+            else if (difInv < 0 && RCFA_Mod.settings.ratioInversion)
+            {
+                result = (float)Math.Round(_curvePoints.Evaluate(difInv * (0.5f + RCFA_Mod.settings.factor)), 2);
+            }
+            __result = result;
         }
     }
 }
--- Source/Settings.cs
+++ Source/Settings.cs
@@ -10,6 +10,8 @@ namespace ResearchCostFactorAdjustment
         public TechLevel threshold = TechLevel.Ultra;
         public float factor = 0.5f;
         public bool exponential = false;
+        public bool ratioInversion = false;
+        public bool overrideTA = true;
 
         public override void ExposeData()
         {
@@ -17,15 +19,13 @@ namespace ResearchCostFactorAdjustment
             Scribe_Values.Look(ref threshold, "threshold", TechLevel.Ultra);
             Scribe_Values.Look(ref factor, "factor", 0.5f);
             Scribe_Values.Look(ref exponential, "exponential", false);
+            Scribe_Values.Look(ref ratioInversion, "ratioInversion", false);
         }
     }
 
     class RCFA_Mod : Mod
     {
         public static RCFA_Settings settings;
-        private static ResearchProjectDef researchProjectDef = new ResearchProjectDef();
-        TextAnchor textAnchor;
-
         public RCFA_Mod(ModContentPack content) : base(content)
         {
             settings = GetSettings<RCFA_Settings>();
@@ -35,93 +35,95 @@ namespace ResearchCostFactorAdjustment
 
         public override void DoSettingsWindowContents(Rect inRect)
         {
+            float columnHeight = 164f;
             Listing_Standard ls = new Listing_Standard();
-            textAnchor = Text.Anchor;
-            researchProjectDef.baseCost = 1000f;
 
-            Rect leftColumn = new Rect(inRect.x, inRect.y, inRect.width / 2f, inRect.height / 2.6f);
-            ls.Begin(leftColumn.ContractedBy(10f));
+            // Left column
+            Rect leftColumn = new Rect(inRect.x, inRect.y, inRect.width / 2f, columnHeight);
+            ls.Begin(leftColumn.ContractedBy(5f));
+            Listing_Standard ls_left = ls.BeginSection(columnHeight - 18f);
             Text.CurFontStyle.fontStyle = FontStyle.Italic;
-            ls.Label("RqRCFA_Label_TechLevelThreshold".Translate(), tooltip: "RqRCFA_Label_TechLevelThresholdDesc".Translate());
+            ls_left.Label("RqRCFA_Label_TechLevelThreshold".Translate(), tooltip: "RqRCFA_Label_TechLevelThresholdDesc".Translate());
             Text.CurFontStyle.fontStyle = FontStyle.Normal;
             for (int i = 2; i < Enum.GetValues(typeof(TechLevel)).Length - 1; i++)
             {
                 TechLevel current = (TechLevel)i;
-                if (ls.RadioButton_NewTemp($"   {current.ToStringHuman().CapitalizeFirst()}", settings.threshold == current))
+                if (ls_left.RadioButton($"   {current.ToStringHuman().CapitalizeFirst()}", settings.threshold == current))
                 {
                     settings.threshold = current;
                 }
             }
-            ls.Gap();
-            Text.CurFontStyle.fontStyle = FontStyle.Italic;
-            ls.Label($"{"RqRCFA_Label_TechLevelFactor".Translate()} {settings.factor}", tooltip: "RqRCFA_Label_TechLevelFactorDesc".Translate());
-            Text.CurFontStyle.fontStyle = FontStyle.Normal;
-            settings.factor = Widgets.HorizontalSlider(ls.GetRect(22f), settings.factor, 0.5f, 5f, roundTo: 0.1f);
+            ls.EndSection(ls_left);
             ls.End();
-
-            Rect rightColumn = new Rect(inRect.width / 2f, inRect.y, inRect.width / 2f, inRect.height / 2.7f);
-            ls.Begin(rightColumn.ContractedBy(10f));
+            // Right column
+            Rect rightColumn = new Rect(inRect.width / 2f, inRect.y, inRect.width / 2f, columnHeight);
+            ls.Begin(rightColumn.ContractedBy(5f));
+            Listing_Standard ls_right = ls.BeginSection(columnHeight - 18f);
             Text.CurFontStyle.fontStyle = FontStyle.Italic;
-            ls.Label("RqRCFA_Label_CostIncrease".Translate(), tooltip: "RqRCFA_Label_CostIncreaseDesc".Translate());
+            ls_right.Label($"{"RqRCFA_Label_TechLevelFactor".Translate()} {settings.factor}", tooltip: "RqRCFA_Label_TechLevelFactorDesc".Translate());
+            //settings.factor = Widgets.HorizontalSlider(ls_right.GetRect(22f), settings.factor, 0.5f, 5f, roundTo: 0.1f);
+            settings.factor = Widgets.HorizontalSlider_NewTemp(ls_right.GetRect(22f), settings.factor, 0.5f, 5f, roundTo: 0.1f);
+            ls_right.Label("RqRCFA_Label_CostIncrease".Translate(), tooltip: "RqRCFA_Label_CostIncreaseDesc".Translate());
             Text.CurFontStyle.fontStyle = FontStyle.Normal;
-            if (ls.RadioButton_NewTemp($"   {"RqRCFA_RB_Linear".Translate()}", !settings.exponential))
+            if (ls_right.RadioButton($"   {"RqRCFA_RB_Linear".Translate()}", !settings.exponential))
                 settings.exponential = false;
-            if (ls.RadioButton_NewTemp($"   {"RqRCFA_RB_Exponential".Translate()}", settings.exponential))
+            if (ls_right.RadioButton($"   {"RqRCFA_RB_Exponential".Translate()}", settings.exponential))
                 settings.exponential = true;
-            Text.Font = GameFont.Medium;
-            Text.Anchor = TextAnchor.MiddleCenter;
             Text.CurFontStyle.fontStyle = FontStyle.Italic;
-            ls.Label(settings.exponential ? "R = B * (1 + D * D * F)" : "R = B * (1 + D * F)", tooltip: "RqRCFA_Label_CostFactorExplanationDesc".Translate());
+            ls_right.Gap(4f);
+            ls_right.CheckboxLabeled("RqRCFA_RatioInversion".Translate(), ref settings.ratioInversion, "RqRCFA_RatioInversionDesc".Translate());
             Text.CurFontStyle.fontStyle = FontStyle.Normal;
-            Text.Anchor = textAnchor;
-            Text.Font = GameFont.Small;
+            ls.EndSection(ls_right);
             ls.End();
-
-            Rect bottomButtons = new Rect(inRect.width / 2f, 170f, inRect.width / 2f, 90f);
-            ls.Begin(bottomButtons.ContractedBy(10f));
-            if (ls.ButtonText("RqRCFA_Button_GameDefault".Translate()))
+            // Table rect
+            Rect table = new Rect(inRect.x, columnHeight + 20f, inRect.width, inRect.height - columnHeight - 50f);
+            DrawTable(table);
+            // Buttons rect
+            Rect bottom = new Rect(inRect.x, inRect.height, inRect.width, 30f);
+            if (ResearchCostFactorAdjustment.TechAdvancingIsActive)
+            {
+                ls.Begin(bottom.LeftPartPixels(2 * bottom.width / 3 - 8f));
+                Color guiColor = GUI.color;
+                GUI.color = Color.Lerp(Color.yellow, Color.grey, 0.7f);
+                Text.CurFontStyle.fontStyle = FontStyle.Italic;
+                ls.Label("RqRCFA_OverrideTechAdvancing".Translate(), tooltip: "RqRCFA_OverrideTechAdvancingDesc".Translate());
+                GUI.color = guiColor;
+                Text.CurFontStyle.fontStyle = FontStyle.Normal;
+                ls.End();
+            }
+            if (Widgets.ButtonText(bottom.RightPartPixels(bottom.width / 3), "RqRCFA_Button_GameDefault".Translate()))
             {
                 settings.factor = 0.5f;
                 settings.threshold = TechLevel.Industrial;
                 settings.exponential = false;
+                settings.ratioInversion = false;
             }
-            if (ls.ButtonText("RqRCFA_Button_ModsDefault".Translate()))
-            {
-                settings.factor = 0.5f;
-                settings.threshold = TechLevel.Ultra;
-                settings.exponential = false;
-            }
-            ls.End();
-
-            Rect bottom = new Rect(inRect.x, inRect.height / 2.8f, inRect.width, inRect.height / 1.5f);
-            DrawTable(bottom);
             settings.Write();
         }
         private void DrawTable(Rect rect)
         {
+            ResearchProjectDef researchProjectDef = new ResearchProjectDef();
+            researchProjectDef.baseCost = 1000f;
             Color guiColor = GUI.color;
+            TextAnchor textAnchor = Text.Anchor;
             GUI.BeginGroup(rect);
-            Rect tableRect = new Rect(30f, 75f, rect.width - 40f, rect.height - 100f);
+            Rect tableRect = new Rect(30f, 65f, rect.width - 40f, rect.height - 90f);
+
+            GUI.color = Color.gray;
+            Text.Anchor = TextAnchor.MiddleLeft;
+            Rect vertLabel = new Rect(tableRect.xMin - Text.LineHeight, tableRect.yMax, tableRect.height / 1.2f, Text.LineHeight * 0.95f);
+            UI.RotateAroundPivot(-90.01f, vertLabel.position);
+            Widgets.Label(vertLabel, "RqRCFA_Label_TechLevelResearcher".Translate());
+            UI.RotateAroundPivot(90.01f, vertLabel.position);
 
             Text.Anchor = TextAnchor.MiddleRight;
-            GUI.color = Color.gray;
+            Rect horLabel = new Rect(tableRect.x + tableRect.width / 6f, tableRect.y - Text.LineHeight, tableRect.width / 1.2f, Text.LineHeight);
+            Widgets.Label(horLabel, "RqRCFA_Label_TechLevelProject".Translate());
+
             Rect tableDesc = new Rect(tableRect.x, tableRect.yMax, tableRect.width, Text.LineHeight);
             Widgets.Label(tableDesc, "RqRCFA_Label_TableDesc".Translate());
 
-            GUI.color = Color.grey;
-            Text.Font = GameFont.Medium;
-            Rect vertLabel = new Rect(tableRect.xMin - Text.LineHeight, tableRect.yMax, tableRect.height / 1.2f, Text.LineHeight * 0.95f);
-            UI.RotateAroundPivot(-90, vertLabel.position);
-            Widgets.Label(vertLabel, "RqRCFA_Label_TechLevelResearcher".Translate());
-            UI.RotateAroundPivot(90, vertLabel.position);
-
-            Text.Anchor = TextAnchor.MiddleLeft;
-            GUI.color = Color.grey;
-            Rect horLabel = new Rect(tableRect.x + tableRect.width / 6f, tableRect.y - Text.LineHeight, tableRect.width / 1.2f, Text.LineHeight);
-            Widgets.Label(horLabel, "RqRCFA_Label_TechLevelProject".Translate());
-            Text.Font = GameFont.Small;
             Text.Anchor = TextAnchor.MiddleCenter;
-
             GUI.BeginGroup(tableRect);
             Widgets.DrawBox(new Rect(0f, 0f, tableRect.width, tableRect.height));
             Widgets.DrawLine(new Vector2(0f, 0f), new Vector2(tableRect.width / 6f, tableRect.height / 6f), Color.grey, 0.5f);
@@ -134,7 +136,7 @@ namespace ResearchCostFactorAdjustment
             GUI.color = new Color(0.8f, 0.8f, 0.8f, 1f);
             for (int i = 1; i < 6; i++)
             {
-                label = $"{((TechLevel)(i + 1)).ToStringHuman().CapitalizeFirst()}";
+                label = $"{((TechLevel)(i + 1)).ToStringHuman().CapitalizeFirst().Truncate(tableRect.width / 6f - 4f)}";
                 Widgets.Label(new Rect(tableRect.width * i / 6f, (tableRect.height / 6f - Text.LineHeight) / 2f, tableRect.width / 6f, Text.LineHeight), label);
                 Widgets.Label(new Rect(0f, tableRect.height * i / 6f + (tableRect.height / 6f - Text.LineHeight) / 2f, tableRect.width / 6f, Text.LineHeight), label);
             }
@@ -145,11 +147,16 @@ namespace ResearchCostFactorAdjustment
                 {
                     var value = (researchProjectDef.baseCost * researchProjectDef.CostFactor((TechLevel)(j + 1)));
                     if (value == researchProjectDef.baseCost)
-                        GUI.color = new Color(0.4f, 0.4f, 0.4f, 1f);
+                        GUI.color = new Color(0.6f, 0.6f, 0.6f, 1f);
+                    else if (value > researchProjectDef.baseCost)
+                    {
+                        float c = 1 / researchProjectDef.CostFactor((TechLevel)(j + 1));
+                        GUI.color = Color.Lerp(Color.red, Color.white, c);
+                    }
                     else
                     {
-                        float c = 1f - researchProjectDef.CostFactor((TechLevel)6) / (researchProjectDef.CostFactor((TechLevel)(j + 1)));
-                        GUI.color = Color.Lerp(Color.white, Color.red, c);
+                        float c = 1.1f - researchProjectDef.CostFactor((TechLevel)(j + 1));
+                        GUI.color = Color.Lerp(Color.white, Color.green, c);
                     }
                     Widgets.Label(new Rect(tableRect.width * i / 6f, tableRect.height * j / 6f + (tableRect.height / 6f - Text.LineHeight) / 2f, tableRect.width / 6f, Text.LineHeight), value.ToString());
                 }
